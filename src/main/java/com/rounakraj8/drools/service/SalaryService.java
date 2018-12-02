7e8a69eb9dee3140ea11fdf69677e3f2605918b3
rounak-sema4 <rounak.raj@sema4genomics.com>
2018-12-02 11:07:19 +0530

    add service to execute rule engine

---
+++ src/main/java/com/rounakraj8/drools/service/SalaryService.java
@@ -0,0 +1,9 @@
+package com.rounakraj8.drools.service;
+
+import com.rounakraj8.drools.payload.Input;
+import com.rounakraj8.drools.payload.Output;
+
+public interface SalaryService {
+
+    Output getSalary(Input input);
+}
