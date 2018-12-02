361453da32ed1ca62e0c6bb55d19fa487fdc308c
Rounak Raj <rounakraj8@gmail.com>
2018-12-02 11:02:07 +0530

    add entry point for SpringBoot

---
+++ src/main/java/com/rounakraj8/drools/DroolsApp.java
@@ -0,0 +1,13 @@
+package com.rounakraj8.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DroolsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsApp.class, args);
+    }
+}
